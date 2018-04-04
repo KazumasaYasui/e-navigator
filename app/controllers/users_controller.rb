1834e6b9864d47543480a9440293f49eee91509a
KazumasaYasui <silver.soul39@gmail.com>
2018-04-04 11:27:08 +0900

    SQL重複発行の再修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,6 @@ class UsersController < ApplicationController
   end
 
   def correct_user
-    # @user = User.find(params[:id])
     if current_user.id != @user.id
       redirect_to root_path
     end
