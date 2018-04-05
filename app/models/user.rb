7c94e45409ba24a393bbd72e2fcc9d50f29d69bc
KazumasaYasui <silver.soul39@gmail.com>
2018-04-05 12:05:35 +0900

    ユーザー削除時に面接も削除されるよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :interviews
+  has_many :interviews, dependent: :destroy
 
   enum sex: { male:0, female:1, other:2 }
 end
