b0251f00e871911b7fe7387ba805d8468ec8edf3
KazumasaYasui <silver.soul39@gmail.com>
2018-04-13 17:15:07 +0900

    ユーザーに紐づく面接を取得する際、メソッドを使用する

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -3,7 +3,7 @@ class InterviewsController < ApplicationController
   before_action :set_interview, only: [:show, :edit, :update, :destroy]
 
   def index
-    @current_user_interviews = Interview.where(user_id: current_user.id).order(created_at: :desc)
+    @current_user_interviews = current_user.interviews.order(interview_datetime: :asc)
     @user = User.find(params[:user_id])
     @interviews = Interview.where(user_id: @user.id)
   end
@@ -30,8 +30,7 @@ class InterviewsController < ApplicationController
 
   def update
     @user = User.find(params[:user_id])
-    @interviews = Interview.where(user_id: @user.id)
-    @interviews.where(interview_status: "approval").update_all(interview_status: "refusal")
+    @user.interviews.where(interview_status: "approval").update_all(interview_status: "refusal")
     if @interview.update(interview_params)
       redirect_to user_interview_path(current_user, @interview), alert: '更新しました。'
     else
